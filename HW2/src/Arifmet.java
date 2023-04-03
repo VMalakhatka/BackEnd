7e6312a71e1282e6566a3e5548ce530c99f631b3
VMalakhatka <vmalakhatka@gmail.com>
2023-04-03 08:37:46 +0200

    HW2 modif

--- HW2/src/Arifmet.java
+++ HW2/src/Arifmet.java
@@ -1,30 +1,39 @@
 public class Arifmet {
-    static int plus(int a , int b){
-        return a+b;
-    }
-    static int minus(int a, int b){
-        return a-b;
-    }
-    static int mul(int a, int b){
-        return a*b;
-    }
-    static int del(int a, int b){
-        return a-b;
+    static int plus(int a, int b) {
+        return a + b;
     }
 
-    static boolean firstMax(a,b,c){
-        if (a<=b) return false;
-        else if(a<=c) return false;
+    static int minus(int a, int b) {
+        return a - b;
+    }
+
+    static int mul(int a, int b) {
+        return a * b;
+    }
+
+    static int del(int a, int b) {
+        return a - b;
+    }
+
+    static boolean firstMax(int a, int b, int c) {
+        if (a <= b) return false;
+        else if (a <= c) return false;
         return true;
     }
-    static boolean firstMore(a,b,c){
-        if (a>b) return true;
-        else if(a>c) return true;
-        return false;
+
+    static boolean firstMore(int a, int b, int c)  {
+//        if (a > b) return true;
+//        else if (a > c) return true;
+//        return false;
+        return  (a>b)? true : (a>c)? true : false;
     }
 
-    static boolean firstMrOne(a,b,c){
-        if (a>b & a<=c) return true;
-        else if(a>c & a<=b) return true;
-        return false;
+//    static boolean firstMrOne(int a, int b, int c)  {
+//        if (a > b & a <= c) return true;
+//        else if (a > c & a <= b) return true;
+//        return false;
+//    }
+    static boolean firstMrOne(int a, int b, int c) {
+        return  (a>b & a<=c)? true : (a>c & a<=b)? true : false;
     }
+}
--- HW2/src/HW2.java
+++ HW2/src/HW2.java
@@ -5,5 +5,10 @@ public class HW2 {
         System.out.println(newCar);
         System.out.println(mazda);
         System.out.println(Arifmet.minus(6,3) + " " + Arifmet.mul(4,6));
+        System.out.println(Arifmet.firstMax(5,3,2));
+        System.out.println(Arifmet.firstMrOne(5,2,7));
+        System.out.println(Arifmet.firstMrOne(5,2,1));
+        System.out.println(Arifmet.firstMrOne(5,6,3));
+        System.out.println(Arifmet.firstMrOne(1,2,3));
     }
 }
